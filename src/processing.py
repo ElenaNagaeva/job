2825e241020309554ed66756f2fd0754b79b068e
Elena <elenanagaevap@gmail.com>
2024-07-14 15:08:16 +0300

    добавлены функции filter_by_state и sort_by_date

--- src/processing.py
+++ src/processing.py
@@ -1,9 +1,13 @@
-def filter_by_state():
+from typing import Any
+def filter_by_state(input_data: list[dict[str]], state_key = 'EXECUTED') -> list[dict[str]]:
+    """Функция фильтрации данных по ключу state"""
+    filter_list = []
+    for key in input_data:
+        if key.get('state') == state_key:
+            filter_list.append(key)
+        return filter_list
 
 
-
-
-
-
-
-def sort_by_date():
+def sort_by_date(input_data: list[dict[str]], reverse=True) -> list[dict[str]]:
+    """Функция сортировки исходных данных по дате"""
+    return sorted(input_data, key=lambda i: i['date'])
