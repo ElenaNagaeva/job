d8a0d23142d2eb364d23136d52b7940936a35f02
Elena <elenanagaevap@gmail.com>
2024-07-14 15:40:43 +0300

    исправлены ошибки

--- src/processing.py
+++ src/processing.py
@@ -1,13 +1,16 @@
 from typing import Any
-def filter_by_state(input_data: list[dict[str]], state_key = 'EXECUTED') -> list[dict[str]]:
+
+
+def filter_by_state(input_data: list[dict[str, Any]], state_key: str = 'EXECUTED') -> list[dict[str, Any]]:
     """Функция фильтрации данных по ключу state"""
     filter_list = []
     for key in input_data:
         if key.get('state') == state_key:
             filter_list.append(key)
-        return filter_list
+    return filter_list
 
 
-def sort_by_date(input_data: list[dict[str]], reverse=True) -> list[dict[str]]:
+def sort_by_date(input_data: list[dict[str, Any]], reverse_list: bool = True) -> list[dict[str, Any]]:
     """Функция сортировки исходных данных по дате"""
-    return sorted(input_data, key=lambda i: i['date'])
+    sorted_list = sorted(input_data, key=lambda d: d.get['date'], reverse=reverse_list)
+    return sorted_list
